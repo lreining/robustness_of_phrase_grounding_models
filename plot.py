10979389259c6276580b33c829736e548eb4a3db
lreining <lars.reining@stud.tu-darmstadt.de>
2024-07-06 09:57:30 +0200

    Finalize plots

--- plot.py
+++ plot.py
@@ -26,10 +26,10 @@ for model in models:
     performances = []
     for manipulation in manipulations:
         performances.append(data[manipulation][model].loc[metric][0])
-    sns.scatterplot(x=manipulations_label, y=performances)
+    sns.scatterplot(x=manipulations_label, y=performances, s=50)
 plt.legend(model_labels)
 plt.xlabel("Manipulation applied to flickr30k", fontdict={"size": 13})
-plt.ylabel(metric, fontdict={"size": 13})
+plt.ylabel("Clean accuracy (Recall@1 all)", fontdict={"size": 13})
 plt.tight_layout()
 plt.savefig("figures/manipulations_recall.pdf")
 plt.show()
@@ -39,13 +39,13 @@ manipulation = "minus1"
 metrics = data['test'][models[0]][data['test'][models[0]].index.str.startswith("Recall@1_")]
 plt.figure(figsize=(9,6))
 for i, model in enumerate(models):
-    diff = data['test'][model].loc[metrics.index].values - data[manipulation][model].loc[metrics.index].values
+    diff = data[manipulation][model].loc[metrics.index].values - data['test'][model].loc[metrics.index].values
     diff = diff.flatten()
     x = [metric[9:].capitalize() for metric in metrics.index]
-    graph = sns.scatterplot(x=x[1:], y=diff[1:], label=model_labels[i])
+    graph = sns.scatterplot(x=x[1:], y=diff[1:], label=model_labels[i], s=50)
 plt.xlabel("Category", fontdict={"size": 13})
-plt.ylabel("Difference in Recall@1 between \n unscrambled and scrambled at word-level ", fontdict={"size": 13})
-plt.legend(loc='lower right', bbox_to_anchor=(0.9, 0))
+plt.ylabel("Robustness (higher is better)", fontdict={"size": 13})
+plt.legend(bbox_to_anchor=(0.9, 0.9))
 plt.tight_layout()
 plt.savefig("figures/phrase_category_recall_diff.pdf")
 plt.show()
@@ -60,7 +60,7 @@ for model in models:
     sns.scatterplot(x=[minus1_perf],y=[filtered_perf])
 sns.regplot(x=minus1_perfs, y=filtered_perfs, scatter=False, line_kws={"color":"black", "lw":1.5, "ls":"--"}, ci=None)
 plt.xlabel("Robustness (Recall@1 with scrambling at word level)")
-plt.ylabel("Clean accuracy (Recall@1)")
+plt.ylabel("Clean accuracy (Recall@1 all)")
 plt.legend(model_labels)
 plt.tight_layout()
 plt.savefig("figures/absolute_robustness_vs_clean.pdf")
@@ -73,11 +73,12 @@ for model in models:
     minus1_perf = data['minus1'][model].loc[metric][0]
     filtered_perfs.append(filtered_perf)
     robustness.append(minus1_perf-filtered_perf)
-    sns.scatterplot(x=[minus1_perf-filtered_perf],y=[filtered_perf])
-sns.regplot(x=robustness, y=filtered_perfs, scatter=False, line_kws={"color":"black", "lw":1.5, "ls":"--"}, ci=None)
-plt.xlabel("Robustness (Recall@1 difference between\nword level scrambling and filtered)")
-plt.ylabel("Clean accuracy (Recall@1)")
+    sns.scatterplot(x=[filtered_perf],y=[minus1_perf-filtered_perf], s=50)
+sns.regplot(x=filtered_perfs, y=robustness, scatter=False, line_kws={"color":"black", "lw":1.5, "ls":"--"}, ci=None)
+plt.xlabel("Clean accuracy (Recall@1 all)")
+plt.ylabel("Robustness (higher is better)")
 plt.legend(model_labels)
 plt.tight_layout()
 plt.savefig("figures/relative_robustness_vs_clean.pdf")
 plt.show()
+# %%
